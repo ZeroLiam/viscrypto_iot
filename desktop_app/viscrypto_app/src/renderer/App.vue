a365f5b25a05916df9bf6bd4e738ebacc1d92eeb
ZeroLiam <liselotr@gmail.com>
2018-12-09 20:18:26 +0100

    Added vue-electron version

---
+++ desktop_app/viscrypto_app/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'viscrypto_desktop'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
