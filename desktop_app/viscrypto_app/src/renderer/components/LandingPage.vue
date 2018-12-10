51afeed2263bda7ecc10f0f7a34b089487352224
ZeroLiam <liselotr@gmail.com>
2018-12-10 16:23:24 +0100

    Working on web app

--- desktop_app/viscrypto_app/src/renderer/components/LandingPage.vue
+++ desktop_app/viscrypto_app/src/renderer/components/LandingPage.vue
@@ -1,29 +1,12 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <img id="logo" src="~@/assets/viscryptologo.png" alt="electron-vue">
     <main>
       <div class="left-side">
         <span class="title">
-          Sup bro
+          Hi there! Upload your image here
         </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
+        <!-- <system-information></system-information> -->
       </div>
     </main>
   </div>
